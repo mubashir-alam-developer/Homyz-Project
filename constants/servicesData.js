6bf83c8b6bbd9f91c02711a40a1551f47eea0693
Beefouru <beefourux@gmail.com>
2024-05-11 22:25:20 +0300

    Updated services

--- constants/servicesData.js
+++ constants/servicesData.js
@@ -2,21 +2,21 @@ export const servicesData = [
   {
     id: 1,
     image: "/Services/images/it-consultancy.jpg",
-    title: "IT Consultancy",
+    title: "GIS Services",
     icon: "/Services/icons/it-consultancy-icon.png",
     shortDescription:
-      "We believe that technology design are revolutionizing brand experiences.",
-    heading: "Tuning in to what really matters",
+      "We provide enterprise GIS solutions,image processing, and strategic advice, GIS system implementation, consultant training and support, Customized application development.",
+    heading: "We provide enterprise GIS solutions, ",
     mainDescription:
       "We believe that technology and human-centered design are revolutionizing brand experiences. Remarkable innovations are allowing products to become more sentient and connected, enabling greater connection between people. Our role is to ensure that each product experience is attuned to people’s needs and relevant to the rhythm and habits of their daily lives. Through first and secondary research, we identify what will really matter to users and we never let go of the vision that inspires great products.",
   },
   {
     id: 2,
     image: "/Services/images/cloud-migration.jpg",
-    title: "Cloud Migration",
+    title: "Physical and Land Use Planning",
     icon: "/Services/icons/cloud-migration-icon.png",
     shortDescription:
-      "Process of designing, creating, deploying and maintaining software.",
+      "We embrace a comprehensive and integrated approach, with local capacity that advocates a broad approach to sustainable development and understands the fundamental role that land use strategies and urban development plays.",
     heading: "Tuning in to what really matters",
     mainDescription:
       "We believe that technology and human-centered design are revolutionizing brand experiences. Remarkable innovations are allowing products to become more sentient and connected, enabling greater connection between people. Our role is to ensure that each product experience is attuned to people’s needs and relevant to the rhythm and habits of their daily lives. Through first and secondary research, we identify what will really matter to users and we never let go of the vision that inspires great products.",
@@ -24,10 +24,10 @@ export const servicesData = [
   {
     id: 3,
     image: "/Services/images/poc-development.jpg",
-    title: "PoC Development",
+    title: "Local Economic Development",
     icon: "/Services/icons/poc-development-icon.png",
     shortDescription:
-      "Protect networks, devices and data from attacks or unauthorized access.",
+      "We endeavour to promote an integrated approach towards physical and land use planning that aims at unlocking and tapping onto the latent economic development opportunities for local benefit in a sustainable manner. .",
     heading: "Tuning in to what really matters",
     mainDescription:
       "We believe that technology and human-centered design are revolutionizing brand experiences. Remarkable innovations are allowing products to become more sentient and connected, enabling greater connection between people. Our role is to ensure that each product experience is attuned to people’s needs and relevant to the rhythm and habits of their daily lives. Through first and secondary research, we identify what will really matter to users and we never let go of the vision that inspires great products.",
@@ -35,9 +35,10 @@ export const servicesData = [
   {
     id: 4,
     image: "/Services/images/smart-team.jpg",
-    title: "Smart Team",
+    title: "Development Control and Property Management",
     icon: "/Services/icons/smart-team-icon.png",
-    shortDescription: "See to the integrity and security of your records.",
+    shortDescription:
+      "This aspect of our scope of service entails all the tools, systems and procedures required in order to manage land and its use effectively.",
     heading: "Tuning in to what really matters",
     mainDescription:
       "We believe that technology and human-centered design are revolutionizing brand experiences. Remarkable innovations are allowing products to become more sentient and connected, enabling greater connection between people. Our role is to ensure that each product experience is attuned to people’s needs and relevant to the rhythm and habits of their daily lives. Through first and secondary research, we identify what will really matter to users and we never let go of the vision that inspires great products.",
@@ -45,9 +46,10 @@ export const servicesData = [
   {
     id: 5,
     image: "/Services/images/quality-assurance.jpg",
-    title: "Quality Assurance",
+    title: "	Capacity Building",
     icon: "/Services/icons/quality-assurance-icon.png",
-    shortDescription: "See to the integrity and security of your records.",
+    shortDescription:
+      "GIS Limited has been actively involved in developing and building capacity for local government structures since its inception and active consultancy in planning and GIS services.",
     heading: "Tuning in to what really matters",
     mainDescription:
       "We believe that technology and human-centered design are revolutionizing brand experiences. Remarkable innovations are allowing products to become more sentient and connected, enabling greater connection between people. Our role is to ensure that each product experience is attuned to people’s needs and relevant to the rhythm and habits of their daily lives. Through first and secondary research, we identify what will really matter to users and we never let go of the vision that inspires great products.",
@@ -55,7 +57,7 @@ export const servicesData = [
   {
     id: 6,
     image: "/Services/images/product-design.jpg",
-    title: "Product Design",
+    title: "Offer in-house services",
     icon: "/Services/icons/product-design-icon.png",
     shortDescription: "See to the integrity and security of your records.",
     heading: "Tuning in to what really matters",
